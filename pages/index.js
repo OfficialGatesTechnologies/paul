2e9851beb7f4a947a4164e26090f83407b6914a5
OfficialGates <gunasekar.k@officialgates.com>
2019-09-06 16:58:42 +0530

    test
    tset

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default withRouter(class Index extends Component {
                     <meta name="publisher" content="officialgates (www.officialgates.com)" />
                 </Head>
 
-              <p>Body Of the content</p>
+              <p>Body Of the content test tset </p>
             </div>
         )
     }
