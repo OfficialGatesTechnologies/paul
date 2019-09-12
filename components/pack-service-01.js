6bc1af067ec3273aa85de17e22252fe46d82034e
OfficialGates <gunasekar.k@officialgates.com>
2019-09-12 14:54:16 +0530

    Tag issue fixed
    Tag issue fixed

--- components/pack-service-01.js
+++ components/pack-service-01.js
@@ -1,38 +1,63 @@
 import React, { Component } from 'react';
 import '../styles/styles.scss';
 import Link from 'next/link';
-
 import { render } from 'react-dom';
 import Tabs from 'react-responsive-tabs';
-
+ 
 import { withRouter } from 'next/router';
 import ReactSVG from 'react-svg';
-
-const presidents = [{ name: 'George Washington', biography: '123' }, { name: 'Theodore Roosevelt', biography: '456' }];
- 
-function getTabs() {
-  return presidents.map((president, index) => ({
-    title: president.name,
-    getContent: () => president.biography,
-    /* Optional parameters */
-    key: index,
-    tabClassName: 'tab',
-    panelClassName: 'panel',
-  }));
-}
-const App = () => <Tabs items={getTabs()} />;
-
+import renderHTML from 'react-render-html';
 export default withRouter(class Header extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            tabItems: [
+                {
+                  
+                    title:'Mobile Apps',
+                    image:'../static/images/icons/tab-i-01.svg',
+                    tabContent:"It is a long established fact that a reader will be distracted by the readable con",
+                    
+                },
+                {
+                    title:'Wearable', 
+                    image:'../static/images/icons/tab-i-02.svg',
+                    tabContent:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'",
 
+                },
+                {
+                    title:'Web', 
+                    image:'../static/images/icons/tab-i-03.svg',
+                    tabContent:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'",
+
+                },
+                {
+                    title:'IOT', 
+                    image:'../static/images/icons/tab-i-04.svg',
+                    tabContent:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'",
+
+                }
+        ]
         }
     }
+
     
     componentDidMount = () => {
     
+    }
+    GenerateSVG = (image,title) => {
+        return <><ReactSVG src={image} /><b>{title}</b></>;
+    }
+    getTabs = () => {
+        return this.state.tabItems.map((president, index) => ({
+            title: this.GenerateSVG(president.image,president.title),
+            getContent: () => renderHTML(president.tabContent), 
+            /* Optional parameters */
+            key: index,
+            tabClassName: 'tab',
+            panelClassName: 'panel',
+          }));
     }
     
     render() {
@@ -41,7 +66,7 @@ export default withRouter(class Header extends Component {
 
             <div>
                 <div className="container">
-                <App/>
+                <Tabs items={this.getTabs()} />
                 </div>
             </div>
         )
